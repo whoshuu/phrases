73f2728389bd0e550b1143a37ac6c41930b9e307
Huu Nguyen <whoshuu@gmail.com>
2016-01-10 14:07:02 -0500

    Use 'use' import pattern for english phrases

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate phrases;
 
+use phrases::english::greetings;
+use phrases::english::farewells;
+
 fn main() {
-    println!("Hello in English: {}", phrases::english::greetings::hello());
-    println!("Goodbye in Japanese: {}", phrases::english::farewells::goodbye());
+    println!("Hello in English: {}", greetings::hello());
+    println!("Goodbye in Japanese: {}", farewells::goodbye());
 
     println!("Hello in Japanese: {}", phrases::japanese::greetings::hello());
     println!("Goodbye in Japanese: {}", phrases::japanese::farewells::goodbye());
